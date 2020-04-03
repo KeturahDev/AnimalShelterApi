1b0ba1fc259011261686667fab13e4a30490fccd
Keturah Howard <keturah.dev@gmail.com>
2020-04-03 09:12:51 -0700

    make minimal changes- confirm 200 status with most basic route

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -3,40 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AnimalShelter.Models;
 
-namespace LocalBuisnessApi.Controllers
+namespace AnimalShelter.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class AnimalsController : ControllerBase
     {
-        // GET api/values
+        // GET api/Animals
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/Animals/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/Animals
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/Animals/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/Animals/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
