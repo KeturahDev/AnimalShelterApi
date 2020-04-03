b0615c4493e64fbfac2b91ec4e4ecc5337214b0c
Keturah Howard <keturah.dev@gmail.com>
2020-04-03 09:05:44 -0700

    create Animal table and migrations folder to create db

---
+++ Migrations/20200403160213_Initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalBuisnessApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    AnimalId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    Personality = table.Column<string>(nullable: true),
+                    PowerStatus = table.Column<int>(nullable: false),
+                    Age = table.Column<int>(nullable: false),
+                    Gender = table.Column<string>(nullable: true),
+                    LookingForSlave = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.AnimalId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
--- Models/Animal.cs
+++ Models/Animal.cs
@@ -1,7 +1,14 @@
-namespace LocalBuisnessApi.Models
+namespace AnimalShelter.Models
 {
-    public class Animal
-    {
-        
-    }
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Personality { get; set; }
+    public int PowerStatus { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+    public bool LookingForSlave { get; set; }
+  }
 }
